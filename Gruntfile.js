ba8fa10f3020cd5ebba9b0708cf1e558d394b7d6
knowledges <qbl378739311@163.com>
2015-08-07 16:40:35 +0800

    配置文件

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+grunt.initConfig({
+	
+// Project configuration.
+pkg: grunt.file.readJSON('package.json'),
+uglify: {
+    options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %>-<%=pkg.version%>.js <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
+    build: {
+        src: 'src/*.js',
+        dest: 'build/js/<%= pkg.name %>-<%=pkg.version%>.min.js'
+    }
+},
+
+jshint:{
+    build:['Gruntfile.js','src/*.js'],
+    options:{
+        jshintrc:'.jshintrc'
+    }
+},
+
+watch: {
+    build: {
+        files: ['src/*'],
+        tasks: ['jshint', 'uglify'],
+        options: {
+            spawn: false
+        }
+    }
+}
+	
+});
+  
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['jshint','uglify','watch']);
+
+};
---
+++ src/pullpage.js
@@ -0,0 +1,16 @@
+(function(window,undefined){
+	function Fullpage(){
+	}
+
+	Fullpage.prototype.init = function(e){
+		var width = window.screen.width,
+			height = window.screen.height;
+
+		
+
+	};
+
+	var full = new Fullpage();
+	full.init();
+
+})(window);
